1a6871e952255640a9a0ab9c86653d3a7f44471c
Kirill Maksimov <kirill@orbs.com>
2018-11-04 12:08:51 +0200

    Add unit test

---
+++ strelets/docker_config_test.go
@@ -0,0 +1,61 @@
+package strelets
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+const expectedDockerConfig = `{
+   "CMD":[
+      "/opt/orbs/orbs-node",
+      "--silent",
+      "--config",
+      "/opt/orbs/config/node.json",
+      "--config",
+      "/opt/orbs/config/network.json",
+      "--log",
+      "/opt/orbs/logs/node.log"
+   ],
+   "ExposedPorts":{
+      "8080/tcp":{
+
+      }
+   },
+   "HostConfig":{
+      "Binds":[
+         "/tmp/root/v1/config/config.json:/opt/orbs/config/node.json",
+         "/tmp/root/v1/config/network.json:/opt/orbs/config/network.json",
+         "/tmp/root/v1/logs:/opt/orbs/logs/"
+      ],
+      "PortBindings":{
+         "8080/tcp":[
+            {
+               "HostIp":"0.0.0.0",
+               "HostPort":"8080"
+            }
+         ]
+      }
+   },
+   "Image":"orbs:export"
+}`
+
+func Test_buildDockerConfig(t *testing.T) {
+	exposedPorts := make(map[string]interface{})
+	exposedPorts["8080/tcp"] = struct{}{}
+
+	portBindings := make(map[string][]portBinding)
+	portBindings["8080/tcp"] = []portBinding{{"0.0.0.0", "8080"}}
+
+	volumes := &virtualChainVolumes{
+		configRoot: "/tmp/root/",
+		config:     "/tmp/root/v1/config/config.json",
+		network:    "/tmp/root/v1/config/network.json",
+		logs:       "/tmp/root/v1/logs",
+	}
+
+	cfg := buildDockerConfig("orbs:export", exposedPorts, portBindings, volumes)
+	jsonConfig, _ := json.Marshal(cfg)
+
+	require.JSONEq(t, expectedDockerConfig, string(jsonConfig), "expected config does not match generated config")
+}

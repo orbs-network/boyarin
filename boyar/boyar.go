a23d957bd817df686cc88618413f5bbeca2fc414
Kirill Maksimov <kirill@orbs.com>
2018-11-20 15:30:33 +0200

    Add string configuration source

--- boyar/boyar.go
+++ boyar/boyar.go
@@ -6,28 +6,39 @@ import (
 )
 
 type configurationSource struct {
-	values []*vchainConfig
+	value configValue
+}
+
+type configValue struct {
+	Keys   map[string]string
+	Chains []*strelets.VirtualChain `json:"chains"`
 }
 
 type ConfigurationSource interface {
+	Keys() []byte
+	Chains() []*strelets.VirtualChain
 }
 
 func NewStringConfigurationSource(input string) (ConfigurationSource, error) {
 	return parseStringConfig(input)
 }
 
-type vchainConfig struct {
-	keys   map[string]string     `json:"keys"`
-	vchain strelets.VirtualChain `json:"vchain"`
-}
-
 func parseStringConfig(input string) (*configurationSource, error) {
-	var cfgs []*vchainConfig
-	if err := json.Unmarshal([]byte(input), &cfgs); err != nil {
+	var value configValue
+	if err := json.Unmarshal([]byte(input), &value); err != nil {
 		return nil, err
 	}
 
 	return &configurationSource{
-		values: cfgs,
+		value: value,
 	}, nil
+}
+
+func (c *configurationSource) Keys() []byte {
+	keys, _ := json.Marshal(c.value.Keys)
+	return keys
+}
+
+func (c *configurationSource) Chains() []*strelets.VirtualChain {
+	return c.value.Chains
 }
--- boyar/boyar_test.go
+++ boyar/boyar_test.go
@@ -1,20 +1,19 @@
 package boyar
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 const input = `
-[
-	{
-		"keys": {
-			"node-public-key": "dfc06c5be24a67adee80b35ab4f147bb1a35c55ff85eda69f40ef827bddec173",
-			"node-private-key": "93e919986a22477fda016789cca30cb841a135650938714f85f0000a65076bd4dfc06c5be24a67adee80b35ab4f147bb1a35c55ff85eda69f40ef827bddec173",
-			"constant-consensus-leader": "dfc06c5be24a67adee80b35ab4f147bb1a35c55ff85eda69f40ef827bddec173"
-		},
-		"vchain": {
+{
+	"keys": {
+		"node-public-key": "dfc06c5be24a67adee80b35ab4f147bb1a35c55ff85eda69f40ef827bddec173",
+		"node-private-key": "93e919986a22477fda016789cca30cb841a135650938714f85f0000a65076bd4dfc06c5be24a67adee80b35ab4f147bb1a35c55ff85eda69f40ef827bddec173",
+		"constant-consensus-leader": "dfc06c5be24a67adee80b35ab4f147bb1a35c55ff85eda69f40ef827bddec173"
+	},
+	"chains": [
+		{
 			"Id":        42,
 			"HttpPort":   8080,
 			"GossipPort": 4400,
@@ -25,18 +24,36 @@ const input = `
 				"Pull":   false
 			}
 		}
-	}
-]
+	]
+}
 `
 
-func Test_BootstrapFromJSON(t *testing.T) {
+func verifySource(t *testing.T, source ConfigurationSource) {
+	require.NotEqual(t, []byte("null"), source.Keys())
+	require.EqualValues(t, 1, len(source.Chains()))
+
+	chain := source.Chains()[0]
+
+	require.EqualValues(t, 42, chain.Id)
+	require.EqualValues(t, 8080, chain.HttpPort)
+	require.EqualValues(t, 4400, chain.GossipPort)
+
+	require.EqualValues(t, "node1", chain.DockerConfig.ContainerNamePrefix)
+	require.EqualValues(t, "506367651493.dkr.ecr.us-west-2.amazonaws.com/orbs-network-v1", chain.DockerConfig.Image)
+	require.EqualValues(t, "master", chain.DockerConfig.Tag)
+	require.EqualValues(t, false, chain.DockerConfig.Pull)
+}
+
+func Test_parseStringConfig(t *testing.T) {
 	source, err := parseStringConfig(input)
 
 	require.NoError(t, err)
-	require.EqualValues(t, 1, len(source.values))
+	verifySource(t, source)
+}
 
-	fmt.Println(fmt.Sprintf("%v", source.values[0]))
+func TestNewStringConfigurationSource(t *testing.T) {
+	source, err := NewStringConfigurationSource(input)
 
-	chain := source.values[0].vchain
-	require.EqualValues(t, 42, chain.Id)
+	require.NoError(t, err)
+	verifySource(t, source)
 }

1ea39560b970127a2217d3e41d0cf7dffd1c1a31
Kirill Maksimov <kirill@orbs.com>
2018-11-27 11:58:33 +0200

    Refactor reverse proxy test

--- test/e2e/e2e_reverse_proxy_test.go
+++ test/e2e/e2e_reverse_proxy_test.go
@@ -13,16 +13,14 @@ import (
 	"time"
 )
 
-func Test_UpdateReverseProxyWithDocker(t *testing.T) {
-	skipUnlessDockerIsEnabled(t)
-
+func testUpdateReverseProxy(t *testing.T, apiProvider func() (adapter.Orchestrator, error)) {
 	server := test.CreateHttpServer("/test", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write([]byte("success"))
 	})
 	server.Start()
 	defer server.Shutdown()
 
-	api, err := adapter.NewDockerAPI("_tmp")
+	api, err := apiProvider()
 	require.NoError(t, err)
 
 	s := strelets.NewStrelets("_tmp", api)
@@ -54,43 +52,18 @@ func Test_UpdateReverseProxyWithDocker(t *testing.T) {
 	}))
 }
 
+func Test_UpdateReverseProxyWithDocker(t *testing.T) {
+	skipUnlessDockerIsEnabled(t)
+
+	testUpdateReverseProxy(t, func() (adapter.Orchestrator, error) {
+		return adapter.NewDockerAPI("_tmp")
+	})
+}
+
 func Test_UpdateReverseProxyWithSwarm(t *testing.T) {
 	skipUnlessSwarmIsEnabled(t)
 
-	server := test.CreateHttpServer("/test", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("success"))
+	testUpdateReverseProxy(t, func() (adapter.Orchestrator, error) {
+		return adapter.NewDockerSwarm()
 	})
-	server.Start()
-	defer server.Shutdown()
-
-	api, err := adapter.NewDockerSwarm()
-	require.NoError(t, err)
-
-	s := strelets.NewStrelets("_tmp", api)
-	chain := chain(1)
-	chain.HttpPort = server.Port()
-
-	chains := []*strelets.VirtualChain{chain}
-	ip := test.LocalIP()
-
-	err = s.UpdateReverseProxy(context.Background(), chains, ip)
-	require.NoError(t, err)
-	defer api.RemoveContainer(context.Background(), "http-api-reverse-proxy")
-
-	require.True(t, test.Eventually(20*time.Second, func() bool {
-		url := fmt.Sprintf("http://%s/%d/test", ip, chain.Id)
-		fmt.Println(url)
-		res, err := http.Get(url)
-		if err != nil {
-			return false
-		}
-		defer res.Body.Close()
-
-		body, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			return false
-		}
-
-		return res.StatusCode == 200 && string(body) == "success"
-	}))
 }

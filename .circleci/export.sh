fa01c5468e598b02bf8146ccd7ff94708bb6b5b5
Kirill Maksimov <kirill@orbs.com>
2019-02-21 18:21:12 +0200

    Fix export.sh

--- .circleci/export.sh
+++ .circleci/export.sh
@@ -3,4 +3,4 @@
 export VERSION=$(git rev-parse HEAD)
 export BOYAR_S3_PATH=s3://orbs-network-releases/infrastructure/boyar/boyar-$VERSION.bin
 
-aws s3 cp --acl public-read --profile admin ./boyar.bin $BOYAR_S3_PATH
+aws s3 cp --acl public-read ./boyar.bin $BOYAR_S3_PATH

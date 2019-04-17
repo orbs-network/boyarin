b1becb1ca727fb1ed08bfc15361a6023b40956c6
Kirill Maksimov <kirill@orbs.com>
2019-04-17 18:06:56 +0300

    Enable static linking

--- build-binaries.sh
+++ build-binaries.sh
@@ -4,4 +4,4 @@ export GIT_COMMIT=$(git rev-parse HEAD)
 export SEMVER=$(cat ./.version)
 export CONFIG_PKG="github.com/orbs-network/boyarin/boyar/config"
 
-time go build -ldflags '-w -extldflags "-static"' -ldflags "-X $CONFIG_PKG.SemanticVersion=$SEMVER -X $CONFIG_PKG.CommitVersion=$GIT_COMMIT" -o boyar.bin -a ./boyar/main/main.go
+time go build -ldflags "-w -extldflags '-static' -X $CONFIG_PKG.SemanticVersion=$SEMVER -X $CONFIG_PKG.CommitVersion=$GIT_COMMIT" -o boyar.bin -a ./boyar/main/main.go

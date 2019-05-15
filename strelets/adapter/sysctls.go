d35276361635dcaf34c4848a6ebf6c0f69e139ed
Kirill Maksimov <kirill@orbs.com>
2019-05-15 11:38:12 +0300

    Bump number of simultaneous connections to the node to 128k

--- strelets/adapter/sysctls.go
+++ strelets/adapter/sysctls.go
@@ -2,7 +2,7 @@ package adapter
 
 func GetSysctls() map[string]string {
 	sysctls := make(map[string]string)
-	sysctls["net.core.somaxconn"] = "25000"
+	sysctls["net.core.somaxconn"] = "128000"
 
 	return sysctls
 }

7544b1840eeaef5863f623c9197973ed89faf4dc
Kirill Maksimov <kirill@orbs.com>
2019-05-28 17:36:39 +0300

    Fix shared network error return value

--- strelets/shared_network.go
+++ strelets/shared_network.go
@@ -12,7 +12,7 @@ type ProvisionSharedNetworkInput struct {
 func (s *strelets) ProvisionSharedNetwork(ctx context.Context, input *ProvisionSharedNetworkInput) error {
 	_, err := s.orchestrator.GetOverlayNetwork(ctx, input.Name)
 	if err != nil {
-		fmt.Errorf("failed to provision shared network %s: %s", input.Name, err)
+		return fmt.Errorf("failed to provision shared network %s: %s", input.Name, err)
 	}
 	return nil
 }

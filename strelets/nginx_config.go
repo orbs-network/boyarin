f5e864581f588f34e8954b5e65260e16cabce8c3
Kirill Maksimov <kirill@orbs.com>
2019-08-29 13:13:00 +0300

    Fix 404 and 502 errors

--- strelets/nginx_config.go
+++ strelets/nginx_config.go
@@ -8,11 +8,20 @@ import (
 )
 
 func getBasicNginxConfig(locations string) string {
-	return fmt.Sprintf(`server { listen 80; %s }`, locations)
+	return fmt.Sprintf(`server {
+listen 80;
+%s
+}`, locations)
 }
 
 func getSSLNginxConfig(locations string) string {
-	return fmt.Sprintf(`server { listen 443; ssl on; ssl_certificate /var/run/secrets/ssl-cert; ssl_certificate_key /var/run/secrets/ssl-key; %s }`, locations)
+	return fmt.Sprintf(`server {
+listen 443;
+ssl on;
+ssl_certificate /var/run/secrets/ssl-cert;
+ssl_certificate_key /var/run/secrets/ssl-key;
+%s
+}`, locations)
 }
 
 type boyar struct {
@@ -31,9 +40,9 @@ type defaultNginxResponse struct {
 	Services services
 }
 
-func getDefaultNginxResponse() string {
+func getDefaultNginxResponse(status string) string {
 	raw, _ := json.Marshal(defaultNginxResponse{
-		Status: "OK",
+		Status: status,
 		Description: "ORBS blockchain node",
 		Services: services{
 			Boyar: boyar{
@@ -45,16 +54,23 @@ func getDefaultNginxResponse() string {
 	return string(raw)
 }
 
+func getNginxLocationDefinition(path string, code int) string {
+	return fmt.Sprintf(`location %s { return %d '%%s'; }`, path, code)
+}
+
 func getNginxLocations(chains []*VirtualChain, ip string) string {
 	locations := []string {
-		fmt.Sprintf(`location / { return 200 '%s'; add_header Content-Type application/json; }`, getDefaultNginxResponse()),
+		fmt.Sprintf(getNginxLocationDefinition("~^/$", 200), getDefaultNginxResponse("OK")),
+		`location / { error_page 404 = @error404; }`,
+		fmt.Sprintf(getNginxLocationDefinition("@error404", 404), getDefaultNginxResponse("Not found")),
+		fmt.Sprintf(getNginxLocationDefinition("@error502", 502), getDefaultNginxResponse("Bad gateway")),
 	}
 
 	for _, chain := range chains {
 		if chain.Disabled {
 			continue
 		}
-		locations = append(locations, fmt.Sprintf(`location /vchains/%d/ { proxy_pass http://%s:%d/; }`, chain.Id, ip, chain.HttpPort))
+		locations = append(locations, fmt.Sprintf(`location /vchains/%d/ { proxy_pass http://%s:%d/; error_page 502 = @error502; }`, chain.Id, ip, chain.HttpPort))
 	}
 
 	return strings.Join(locations, "\n")

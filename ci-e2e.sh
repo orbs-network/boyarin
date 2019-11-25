9a6072cab4a7fb9cda8d56b8221bc7391a448f1d
amir-arad <amir@orbs.com>
2019-11-25 15:25:02 +0200

    make e2e job not use docker

--- ci-e2e.sh
+++ ci-e2e.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -x
 
-./docker-build.sh
+GO_VERSION=1.12.9
+
+curl -sSL "https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz" | sudo tar -xz -C /usr/local/
+PATH=$PATH:/usr/local/go/bin # echo "export PATH=$PATH:/usr/local/go/bin" >> $BASH_ENV
+
+go get
+env
+go env
+
 $(aws ecr get-login --no-include-email --region us-west-2)
 docker pull $ORBS_NODE_DOCKER_IMAGE:master
 docker pull $SIGNER_DOCKER_IMAGE:master
@@ -11,6 +19,18 @@ docker rm -f $(docker ps -aq)
 docker service rm $(docker service ls -q)
 docker secret rm $(docker secret ls -q)
 
+GANACHE_ID=$(docker run -p "7545:7545" -d trufflesuite/ganache-cli -m 'pet talent sugar must audit chief biology trash change wheat educate bone' -i 5777 -p 7545)
+
+export ENABLE_ETHEREUM='true'
+export ETHEREUM_PRIVATE_KEY='7a16631b19e5a7d121f13c3ece279c10c996ff14d8bebe609bf1eca41211b291'
+export ETHEREUM_ENDPOINT='http://ganache:7545'
+export ENABLE_SWARM='true'
 export LOCAL_IP=$(python -c "import socket; print socket.gethostbyname(socket.gethostname())")
 
-docker-compose -f docker-compose.test.yml up --abort-on-container-exit --exit-code-from boyar
+# sh sleep 5
+# rm -rf _tmp
+go test ./... -v
+RESULT=$?
+
+docker rm -fv $GANACHE_ID
+exit $RESULT

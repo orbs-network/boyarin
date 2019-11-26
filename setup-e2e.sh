e67fd0e52b400ef11e81b22ca8742d60d7fe8816
amir-arad <amir@orbs.com>
2019-11-26 09:42:03 +0200

    pretty test results in ci

--- setup-e2e.sh
+++ setup-e2e.sh
@@ -1,25 +1,40 @@
 #!/bin/bash -x
 
+# install go and gotestsum (only in CI)
 GO_VERSION=1.12.9
 if [ "${CI:-false}" = "true" ]; then
   sudo rm -rf $(dirname $(dirname $(which go))) # remove go base dir (/usr/local/go)  to avoid src duplications
   curl -sSL "https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz" | sudo tar -xz -C /usr/local/
   curl -sSL "https://github.com/gotestyourself/gotestsum/releases/download/v0.4.0/gotestsum_0.4.0_linux_amd64.tar.gz" | sudo tar -xz -C /usr/local/bin
 
+  # ensure /usr/local/go/bin is in path (in this step and the next)
   echo "export PATH=$PATH:/usr/local/go/bin" >> $BASH_ENV
   PATH=$PATH:/usr/local/go/bin
 fi
 
+# print environment to log
 go version
 env
 go env
 
+# setup
 $(aws ecr get-login --no-include-email --region us-west-2)
-docker pull $ORBS_NODE_DOCKER_IMAGE:master
-docker pull $SIGNER_DOCKER_IMAGE:master
-docker tag $ORBS_NODE_DOCKER_IMAGE:master orbs:export
-docker tag $SIGNER_DOCKER_IMAGE:master orbs:signer
+docker pull ${ORBS_NODE_DOCKER_IMAGE}:master
+docker pull ${SIGNER_DOCKER_IMAGE}:master
+docker tag ${ORBS_NODE_DOCKER_IMAGE}:master orbs:export
+docker tag ${SIGNER_DOCKER_IMAGE}:master orbs:signer
 docker swarm init
-docker rm -f $(docker ps -aq)
-docker service rm $(docker service ls -q)
-docker secret rm $(docker secret ls -q)
+
+# clean docker state
+DOCKER_INSTANCES=$(docker ps -aq)
+DOCKER_SERVICES=$(docker service ls -q)
+DOCKER_SECRETS=$(docker secret ls -q)
+if [ -z "${DOCKER_INSTANCES}" ]; then
+  docker rm -f ${DOCKER_INSTANCES}
+fi
+if [ -z "${DOCKER_SERVICES}" ]; then
+  docker service rm ${DOCKER_SERVICES}
+fi
+if [ -z "${DOCKER_SECRETS}" ]; then
+  docker secret rm ${DOCKER_SECRETS}
+fi

b502e23c65e98fda25ba1fc2e599d2fa953b87e0
amir-arad <amir@orbs.com>
2019-11-26 18:33:58 +0200

    fix nvm init

--- .circleci/setup-machine.sh
+++ .circleci/setup-machine.sh
@@ -22,9 +22,12 @@ if ! [ -x "$(command -v nvm)" ]; then
   echo "nvm not installed. installing..."
   $(curl -sSL "https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh") # install nvm
 fi
-LOAD_NVM_LINE="\nexport NVM_DIR=\"${NVM_DIR}\"\n[ -s \"\$NVM_DIR/nvm.sh\" ] && \\. \"\$NVM_DIR/nvm.sh\"  # This loads nvm\n"
 
-echo $LOAD_NVM_LINE >> $BASH_ENV # ensure nvm is loaded in the next step
+LOAD_NVM_CMD="\n
+export NVM_DIR=\"${NVM_DIR}\"\n
+[ -s \"\$NVM_DIR/nvm.sh\" ] && \\. \"\$NVM_DIR/nvm.sh\"  # This loads nvm\n"
+
+printf $LOAD_NVM_CMD >> $BASH_ENV # ensure nvm is loaded in the next step
 
 # install node
 # https://www.cloudesire.com/how-to-upgrade-node-on-circleci-machine-executor/

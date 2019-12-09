15aaf3b5dcdb21226f9eccc417168744341517a4
amir-arad <amir@orbs.com>
2019-12-09 17:00:00 +0200

    limit max connections to 65535 (unsigned 16 bit integer) to support older kernels

--- strelets/adapter/sysctls.go
+++ strelets/adapter/sysctls.go
@@ -2,7 +2,7 @@ package adapter
 
 func GetSysctls() map[string]string {
 	sysctls := make(map[string]string)
-	sysctls["net.core.somaxconn"] = "128000"
+	sysctls["net.core.somaxconn"] = "65535"
 
 	return sysctls
 }

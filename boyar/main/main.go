faf6a90fd7cd09ba0e5057df21310d66b02b1d96
amir-arad <amir@orbs.com>
2019-12-15 14:24:04 +0200

    extract boyar loop to service

--- boyar/main/main.go
+++ boyar/main/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/orbs-network/boyarin/boyar"
 	"github.com/orbs-network/boyarin/boyar/config"
 	"github.com/orbs-network/boyarin/services"
 	"github.com/orbs-network/boyarin/strelets/adapter"
-	"github.com/orbs-network/boyarin/supervized"
 	"github.com/orbs-network/boyarin/version"
 	"github.com/orbs-network/scribe/log"
 	"os"
@@ -116,47 +113,7 @@ func execute(flags *config.Flags, logger log.Logger) error {
 
 	services.WatchAndReportServicesStatus(logger)
 
-	runCoreBoyarService(flags, logger)
+	services.RunCoreBoyarService(flags, logger)
 
 	return nil
-}
-
-const BOYAR_CONFIG_HASH = "boyar config"
-
-func runCoreBoyarService(flags *config.Flags, logger log.Logger) {
-	// Even if something crashed, things still were provisioned, meaning the cache should stay
-	configCache := config.NewCache()
-	<-supervized.GoForever(func(first bool) {
-		defer func() {
-			<-time.After(flags.PollingInterval)
-		}()
-		cfg, err := config.GetConfiguration(flags)
-		if err != nil {
-			logger.Error("invalid configuration", log.Error(err))
-			return
-		}
-		hash := cfg.Hash()
-		if hash == configCache.Get(BOYAR_CONFIG_HASH) {
-			logger.Error("configuration has not changed")
-			return
-		}
-		// random delay when provisioning change (that is, not bootstrap flow)
-		if !first {
-			randomDelay(cfg, flags.MaxReloadTimeDelay, logger)
-		}
-		ctx, cancel := context.WithTimeout(context.Background(), flags.Timeout)
-		defer cancel()
-
-		err = boyar.Flow(ctx, cfg, configCache, logger)
-		if err != nil {
-			logger.Error("error during execution", log.Error(err))
-		}
-		configCache.Put(BOYAR_CONFIG_HASH, hash)
-	})
-}
-
-func randomDelay(cfg config.NodeConfiguration, maxDelay time.Duration, logger log.Logger) {
-	reloadTimeDelay := cfg.ReloadTimeDelay(maxDelay)
-	logger.Info("waiting to apply new configuration", log.String("delay", maxDelay.String()))
-	<-time.After(reloadTimeDelay)
 }

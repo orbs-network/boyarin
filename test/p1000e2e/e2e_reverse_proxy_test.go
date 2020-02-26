109527083fe0d63bf4c5115b723bca5ea0a5d9b8
Kirill Maksimov <kirill@orbs.com>
2020-02-26 11:43:09 +0200

    Add test for SSL

---
+++ test/p1000e2e/e2e_reverse_proxy_test.go
@@ -0,0 +1,48 @@
+package p1000e2e
+
+import (
+	"context"
+	"fmt"
+	"github.com/orbs-network/boyarin/test/helpers"
+	"github.com/orbs-network/govnr"
+	"github.com/orbs-network/scribe/log"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestE2ERunSingleVirtualChainWithSSL(t *testing.T) {
+	vc1 := VChainArgument{Id: 42}
+	helpers.WithContextAndShutdown(func(ctx context.Context) (waiter govnr.ShutdownWaiter) {
+		logger := log.GetLogger()
+		helpers.InitSwarmEnvironment(t, ctx)
+		keys := KeyConfig{
+			NodeAddress:    PublicKey,
+			NodePrivateKey: PrivateKey,
+		}
+
+		flags, cleanup := SetupBoyarDependencies(t, keys, vc1)
+		defer cleanup()
+
+		flags.SSLCertificatePath = "./fixtures/cert.pem"
+		flags.SSLPrivateKeyPath = "./fixtures/key.pem"
+
+		waiter = InProcessBoyar(t, ctx, logger, flags)
+
+		helpers.RequireEventually(t, 20*time.Second, func(t helpers.TestingT) {
+			AssertVchainUp(t, 80, PublicKey, vc1)
+
+			url := fmt.Sprintf("https://127.0.0.1:443/vchains/%d", vc1.Id)
+			fmt.Println(url)
+
+			client := http.Client{
+				Timeout: 2 * time.Second,
+			}
+			_, err := client.Get(url)
+			require.Error(t, err)
+			require.Errorf(t, err, "Get %s: x509: cannot validate certificate for 127.0.0.1 because it doesn't contain any IP SANs", url)
+		})
+		return
+	})
+}

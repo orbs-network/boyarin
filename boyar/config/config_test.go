9ab536c04deb91f0c8741f32c81bacb5b02a5c45
Kirill Maksimov <kirill@orbs.com>
2020-03-13 01:12:44 +0200

    Fix config tests

--- boyar/config/config_test.go
+++ boyar/config/config_test.go
@@ -49,7 +49,7 @@ func Test_StringConfigurationSource(t *testing.T) {
 	require.NotNil(t, source.Services())
 	require.NotEmpty(t, source.Chains()[0].Config["signer-endpoint"])
 
-	require.EqualValues(t, 8080, source, source.Chains()[0].InternalHttpPort)
+	require.EqualValues(t, 8080, source.Chains()[0].InternalHttpPort)
 }
 
 func Test_StringConfigurationSourceFromEmptyConfig(t *testing.T) {

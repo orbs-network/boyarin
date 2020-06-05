b550f1e522ef693d046938f16c367d4fbc37be0c
itamar Arjuan <itamar.arjuan@gmail.com>
2020-06-05 16:39:49 +0300

    testing creds setting

--- .circleci/set-aws-credentials.sh
+++ .circleci/set-aws-credentials.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/.aws
 
 echo "[default]
-aws_access_key_id = $AWS_ACCESS_KEY_ID
-aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" > ~/.aws/credentials
+aws_access_key_id=$AWS_ACCESS_KEY_ID
+aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" > ~/.aws/credentials
 
 ls -lh ~/.aws

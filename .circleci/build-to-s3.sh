a10ff5e641a2507821301058d00d196af94eff4b
itamar Arjuan <itamar.arjuan@gmail.com>
2020-06-05 17:36:01 +0300

    re-instating the script

--- .circleci/build-to-s3.sh
+++ .circleci/build-to-s3.sh
@@ -2,16 +2,10 @@
 
 GITHASH=$(./.circleci/hash.sh)
 
-ls -lh ~/.aws
-
-echo "Testing S3 access.."
-aws s3 ls
-aws s3 ls s3://boyar-dev-releases
-
-exit 0
-
 echo "Building Boyar for git commit ${GITHASH}.."
 ./build-binaries.sh
 
 echo "Uploading to the dev bucket.."
 aws s3 cp --acl public-read boyar.bin s3://boyar-dev-releases/boyar/boyar-$GITHASH.bin
+
+echo "Built and uploaded!"

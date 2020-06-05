c7a62a72f5b3bc6e4d2db440cd3e77a632578ae7
itamar Arjuan <itamar.arjuan@gmail.com>
2020-06-05 15:17:48 +0300

    removing the time command

--- build-binaries.sh
+++ build-binaries.sh
@@ -3,4 +3,4 @@
 export SEMVER=$(cat ./.version)
 export CONFIG_PKG="github.com/orbs-network/boyarin/version"
 
-time go build -ldflags "-w -extldflags '-static' -X $CONFIG_PKG.SemanticVersion=$SEMVER -X $CONFIG_PKG.CommitVersion=$GIT_COMMIT" -o boyar.bin -a ./boyar/main/main.go
+go build -ldflags "-w -extldflags '-static' -X $CONFIG_PKG.SemanticVersion=$SEMVER -X $CONFIG_PKG.CommitVersion=$GIT_COMMIT" -o boyar.bin -a ./boyar/main/main.go

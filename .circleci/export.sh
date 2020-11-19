28555b228cd88f15efddc1f94a865f75cb5baede
Kirill Maksimov <kirill@orbs.com>
2020-11-19 18:20:06 +0200

    Fix exports script

--- .circleci/export.sh
+++ .circleci/export.sh
@@ -3,4 +3,4 @@
 export VERSION=$(cat .version)
 export BOYAR_S3_PATH=s3://orbs-network-releases/infrastructure/boyar/boyar-$VERSION.bin
 
-aws s3 cp --acl public-read --recursive ./_bin $BOYAR_S3_PATH
+aws s3 cp --acl public-read ./_bin/boyar-$VERSION.bin $BOYAR_S3_PATH
